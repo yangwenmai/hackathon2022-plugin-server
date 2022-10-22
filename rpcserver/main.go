c251ac503e9a0202906d9bb0d0b4de87037cc22c
maiyang <yangwen.yw@gmail.com>
2022-10-22 18:21:39 +0800

    feat: http and rpc server for cdc plugin debug

---
+++ rpcserver/main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/pingcap/tiflow/cdc/model"
+	"log"
+	"net"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+)
+
+var (
+	sinkAddTable             = "sink_add_table"
+	sinkRemoveTable          = "sink_remove_table"
+	sinkEmitRowChangedEvents = "sink_emit_row_changed_events"
+	sinkEmitDDLEvent         = "sink_emit_ddl_event"
+)
+
+type SinkSyncArgs struct {
+	Operation string      `json:"operation"`
+	Data      interface{} `json:"data"`
+}
+
+type Response struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+// AddTableReq add table request
+type AddTableReq struct {
+	TableID int64 `json:"table_id"`
+}
+
+// RemoveTableReq remove table request
+type RemoveTableReq struct {
+	TableID int64 `json:"table_id"`
+}
+
+type SinkSyncService struct{}
+
+func (t *SinkSyncService) SinkSync(args *SinkSyncArgs, result *Response) error {
+	*result = Response{Code: 200, Message: fmt.Sprintf("Sink sync data %v", args)}
+	log.Printf("SinkSync: %v", result)
+
+	rd, err := json.Marshal(args.Data)
+	if err != nil {
+		log.Printf("marshal failed, error: %v", err)
+		return err
+	}
+	switch args.Operation {
+	case sinkAddTable:
+		AddTable(rd)
+	case sinkRemoveTable:
+		RemoveTable(rd)
+	case sinkEmitRowChangedEvents:
+		EmitRowChangedEvents(rd)
+	case sinkEmitDDLEvent:
+		EmitDDLEvent(rd)
+	default:
+		log.Printf("unsupport operation")
+		result.Code = 400
+		result.Message = "unsupport operation"
+	}
+	return nil
+}
+
+func main() {
+	rpcServer := rpc.NewServer()
+	sss := new(SinkSyncService)
+
+	rpcServer.Register(sss)
+	listener, _ := net.Listen("tcp", "127.0.0.1:5006")
+	log.Printf("rpc server start")
+	for {
+		conn, _ := listener.Accept()
+		go rpcServer.ServeCodec(jsonrpc.NewServerCodec(conn))
+	}
+}
+
+func AddTable(data []byte) {
+	log.Printf("start execute add table")
+	atr := &AddTableReq{}
+	err := json.Unmarshal(data, atr)
+	if err != nil {
+		log.Printf("addTable unmarshal failed, error: %v", err)
+		return
+	}
+	log.Printf("req data: %v", atr)
+	log.Printf("execute add table end")
+}
+
+func RemoveTable(data []byte) {
+	log.Printf("start execute remove table")
+	rtr := &RemoveTableReq{}
+	err := json.Unmarshal(data, rtr)
+	if err != nil {
+		log.Printf("RemoveTable unmarshal failed, error: %v", err)
+		return
+	}
+	log.Printf("req data: %v", rtr)
+	log.Printf("execute remove table end")
+}
+
+func EmitRowChangedEvents(data []byte) {
+	log.Printf("start execute row changed events")
+	rces := []*model.RowChangedEvent{}
+	err := json.Unmarshal(data, &rces)
+	if err != nil {
+		log.Printf("EmitRowChangedEvents unmarshal failed, error: %v", err)
+		return
+	}
+	for i, rce := range rces {
+		log.Printf("index: %v, table: %v", i, rce.Table)
+		for j, column := range rce.Columns {
+			log.Printf("column index: %v, column value: %v", j, column.Value)
+		}
+	}
+	log.Printf("execute row changed events end")
+}
+
+func EmitDDLEvent(data []byte) {
+	log.Printf("start execute ddl event")
+	ddlEvent := &model.DDLEvent{}
+	err := json.Unmarshal(data, ddlEvent)
+	if err != nil {
+		log.Printf("RemoveTable unmarshal failed, error: %v", err)
+		return
+	}
+	log.Printf("req data: %v", ddlEvent)
+	log.Printf("execute ddl event end")
+}
